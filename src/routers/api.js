a8c98c380ae348c8990d38b29362fdbc54f45243
hyangwoosh <shine.thw@gmail.com>
2021-11-30 14:07:14 +0800

    Get user profile

--- src/routers/api.js
+++ src/routers/api.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
 const postsRouter = require('./posts');
+<<<<<<< HEAD
 const usersRouter = require('./users')
 const profileRouter = require('./profile');
 
 
 router.use('/posts', postsRouter);
 router.use('/users', usersRouter);
+=======
+const profileRouter = require('./profile');
+
+router.use('/posts', postsRouter);
+>>>>>>> 75b4a98 (Jerald - Get user profile)
 router.use('/profile', profileRouter);
 module.exports = router;
--- src/routers/profile.js
+++ src/routers/profile.js
@@ -2,17 +2,27 @@ const router = require('express').Router();
 const connection = require('../database/database');
 
 // Update specific user
+<<<<<<< HEAD
 // GET user profile
+=======
+// Retrieve specific user profile by ID? (GET)
+>>>>>>> 75b4a98 (Jerald - Get user profile)
 router.get('/getUsers/:username', function (req, res) {
     var username = req.params.username;
 
     const getUsersQuery = {
+<<<<<<< HEAD
         text: 'SELECT * FROM users WHERE username = $1;',
         values: [username],
+=======
+      text: 'SELECT * FROM users WHERE username = $1;',
+      values: [username],
+>>>>>>> 75b4a98 (Jerald - Get user profile)
     };
 
     connection.query(getUsersQuery, function (error, result) {
         if (error) {
+<<<<<<< HEAD
             // console.log(error);
             res.status(500).json({
                 error: 'Error while retrieving profile'
@@ -33,4 +43,92 @@ router.get('/getUsers/:username', function (req, res) {
         }
     });
 });
+=======
+          // console.log(error);
+          res.status(500).json({
+            error: 'Error while retrieving profile'
+          });
+        } else {
+          if (result) {
+            // console.log(result);
+            res.status(200).json({
+              message: 'Retrieved profile successfully',
+              result: result.rows
+            });
+          } else {
+            // console.log(error);
+            res.status(404).json({
+              error: `Profile not found`
+            });
+          }
+        }
+      });
+    });
+
+// Retrieve email
+router.get('/getEmail', function (req, res) {
+  const getUsersQuery = {
+      text: 'SELECT * FROM users WHERE email = $2',
+  };
+
+  connection.query(getUsersQuery, function (error, result) {
+      if (error) {
+        // console.log(error);
+        res.status(500).json({
+          error: 'Error while retrieving profile'
+        });
+      } else {
+        if (result) {
+          // console.log(result);
+          res.status(200).json({
+            message: 'Retrieved profile successfully',
+            result: result.rows
+          });
+        } else {
+          // console.log(error);
+          res.status(404).json({
+            error: `Profile not found`
+          });
+        }
+      }
+    });
+  });
+  
+// retrieve password
+
+// update username
+router.put('/:username', function (req, res) {
+  var userid = req.params.userid;
+
+  const updatePostQuery = {
+    text: 'UPDATE email SET title=$1, content=$2, updated_at=$3 WHERE post_id=$4;',
+    values: [emailID],
+  };
+
+  connection.query(updatePostQuery, function (error, result) {
+    if (error) {
+      // console.log(error);
+      res.status(500).json({
+        error: 'Error while updating post'
+      });
+    } else {
+      // console.log(result);
+      if (result.rowCount === 1) {
+        res.status(200).json({
+          message: 'Updated post successfully'
+        });
+      } else {
+        // console.log(error);
+        res.status(404).json({
+          error: `Post ID ${postID} not found`
+        });
+      }
+    }
+  });
+});
+
+// update password
+router.put
+
+>>>>>>> 75b4a98 (Jerald - Get user profile)
 module.exports = router;
