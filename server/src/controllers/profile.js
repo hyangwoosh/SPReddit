96544862636be1e29d9f465bc8e3ae13dee18d7c
hyangwoosh <shine.thw@gmail.com>
2022-02-04 17:11:24 +0800

    Implemented jwt and merged code

--- server/src/controllers/profile.js
+++ server/src/controllers/profile.js
@@ -1,8 +1,39 @@
+require('dotenv').config();
 const router = require('express').Router();
 const connection = require('../database/database');
+const jwt = require('jsonwebtoken'); 
 
-// Update specific user
 // GET user profile
+router.get('/getinfo', function(req, res) {
+  var jwtfetch = req.query.jwt
+
+  jwt.verify(jwtfetch, process.env.ACCESS_TOKEN_SECRET, function(err, jwt) {
+    
+    const sql = "SELECT * FROM users WHERE email = ($1)";
+    const values = [jwt.email];
+
+    connection.query(sql, values, (error, result) => {
+
+      if (error != null) {
+  
+        if (error) {
+          res.send(error);
+        }
+      } else if (result != null) {
+  
+        if (result.rows.length == 0) {
+  
+          res.send({ "message": `Information not found` });
+        } else {
+
+            res.send({ "username": result.rows[0].username });
+        }
+      }
+    })
+
+  });
+})
+
 router.get('/getUsers/:id', function (req, res) {
   const id = req.params.id;
 
@@ -67,5 +98,37 @@ router.put('/updateUser/:id', function (req, res) {
     }
   });
 });
+
+router.get('/userpost', function (req, res) {
+  var id = req.params.id;
+  Authorization: Bearer
+
+  const getUsersQuery = {
+    text: 'SELECT * FROM users WHERE id = $1;',
+    values: [id],
+  };
+
+  connection.query(getUsersQuery, function (error, result) {
+      if (error) {
+        // console.log(error);
+        res.status(500).json({
+          error: 'Error while retrieving profile'
+        });
+      } else {
+        if (result) {
+          // console.log(result);
+          res.status(200).json({
+            message: 'Retrieved profile successfully',
+            result: result.rows
+          });
+        } else {
+          // console.log(error);
+          res.status(404).json({
+            error: `Profile not found`
+          });
+        }
+      }
+    });
+  });
 
 module.exports = router;
